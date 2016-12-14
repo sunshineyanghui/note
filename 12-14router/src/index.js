a4c02bd2481b8c26d81e3e1fd22b937d00d2276b
sunshineyanghui <1187073986@qq.com>
2016-12-14 16:30:48 +0800

    react-router

--- 12-14router/src/index.js
+++ 12-14router/src/index.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends Component {
+// import App from './App';
+import {Link} from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
+class App extends React.Component {
   render(){
     return(
       <div>
-        Hello
+        <Link to='/hello1'>Hello1</Link>
+        <Link to='/hello2'>Hello2</Link>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+class Hello1 extends React.Component {
+  render(){
+    return(
+      <h1>
+        Hello1 Page
+      </h1>
+    )
+  }
+}
+
+class Hello2 extends React.Component {
+  render(){
+    return(
+      <h1>
+        Hello2 Page
+      </h1>
+    )
+  }
+}
+
+const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <Route path='/' component={App} />
+    <Route path='/hello1' component={Hello1} />
+    <Route path='/hello2' component={Hello2} />
+  </Router>
+);
+ReactDOM.render(renderRoutes(), document.getElementById('app'));
